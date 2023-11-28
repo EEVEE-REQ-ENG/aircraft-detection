b4f60e4a106e88e5c92cad31bebd1dd8f69f40a0
Ena <enachealexandru00@yahoo.com>
2023-11-28 20:59:19 +0200

    Add sample images and file sequence generator for processed images
    - Updated .gitignore
    - Updated README.md with installation commands

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-### Install tkinter
-- in PyCharm: File > Settings > Python Interpreter > Add 'future' package
-
 ### Install Python 3.9.13
+
+### Install dependencies
+
+- in PyCharm: File > Settings > Python Interpreter > Add 'future' package
+- pip install matplotlib
+- pip install keras
+- pip install tensorflow
+- pip install opencv-contrib-python
--- aircraft_detector.py
+++ aircraft_detector.py
@@ -1,13 +1,17 @@
-import os, cv2
-import numpy as np
+import cv2
 import matplotlib.pyplot as plt
+import numpy as np
 from keras.models import load_model
+
 
 class Model:
     def __init__(self):
+        self.__model_final = None
         self.load_model()
+
     def load_model(self):
         self.__model_final = load_model('ieeercnn_vgg16_1.h5')
+
     def process_image(self, path):
         cv2.setUseOptimized(True)
         ss = cv2.ximgproc.segmentation.createSelectiveSearchSegmentation()
@@ -21,14 +25,27 @@ class Model:
         for e, result in enumerate(ssresults):
             if e < 2000:
                 x, y, w, h = result
-                timage = imout[y:y+h, x:x+w]
+                timage = imout[y:y + h, x:x + w]
                 resized = cv2.resize(timage, (224, 224), interpolation=cv2.INTER_AREA)
                 img = np.expand_dims(resized, axis=0)
                 out = self.__model_final.predict(img)
                 if out[0][0] > 0.70:
-                    cv2.rectangle(imout, (x, y), (x+w, y+h), (0, 255, 0), 1, cv2.LINE_AA)
+                    cv2.rectangle(imout, (x, y), (x + w, y + h), (0, 255, 0), 1, cv2.LINE_AA)
         fig = plt.gcf()
         plt.axis('off')
         plt.imshow(imout)
-        fig.savefig("result.png", bbox_inches='tight')
-        return "result.png"
+        result_filename = 'result_' + str(get_file_sequence()) + '.png'
+        fig.savefig(result_filename, bbox_inches='tight')
+        return result_filename
+
+
+def get_file_sequence():
+    reader = open('file_sequence.txt', 'r')
+    current_sequence = int(reader.readline())
+    reader.close()
+
+    writer = open('file_sequence.txt', 'w')
+    writer.write(str(current_sequence + 1))
+    writer.close()
+
+    return current_sequence
